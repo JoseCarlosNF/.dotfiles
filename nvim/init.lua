8400a357767668b9b1e2d5e5003c63728e797468
JoseCarlosNF <josecarlos.n.f99@gmail.com>
2023-02-19 18:16:48 -0300

    refactor(nvim/init): converte as configurações do neovim para lua
    de init.vim para init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,19 +5,46 @@ vim.opt.softtabstop     = 2
 vim.opt.shiftwidth      = 2
 vim.opt.expandtab       = true
 vim.opt.nrformats       = 'alpha'
+vim.opt.ignorecase      = true
 
 vim.opt.encoding        = 'utf-8'
 vim.opt.textwidth       = 80
 vim.opt.cursorline      = true
-vim.wo.number           = true
+vim.opt.number          = true
 vim.opt.relativenumber  = true
 vim.opt.linebreak       = true
 vim.opt.mouse           = 'a'
+vim.opt.scrolloff       = 3
+vim.opt.swapfile        = false
+vim.opt.updatetime      = 300
+vim.opt.undofile        = true
+vim.opt.clipboard       = 'unnamedplus'
 
 vim.opt.foldmethod      = 'expr'
 -- vim.opt.foldexpr        = 'nvim_treesitter#foldexpr()'
 vim.opt.termguicolors   = true
 vim.opt.background      = 'dark'
+vim.opt.conceallevel    = 2
+
+
+--------------------------------- Spell check ---------------------------------
+
+vim.api.nvim_create_autocmd(
+  {'BufNewFile', 'BufRead'},
+  {
+    pattern = {'*.md'},
+    command = 'setlocal spell spelllang=pt,en'
+  }
+)
+vim.api.nvim_set_hl(0, 'SpellRare', { cterm=underline, ctermfg=White, ctermbg=Yellow })
+vim.api.nvim_set_hl(0, 'SpellLocal', { cterm=underline, ctermfg=White, ctermbg=Green })
+vim.api.nvim_set_hl(0, 'SpellCap', { cterm=underline, ctermfg=White, ctermbg=DarkBlue })
+vim.api.nvim_set_hl(0, 'SpellBad', { cterm=underline, ctermfg=White, ctermbg=DarkRed })
+
+----------------------------------- Mapping -----------------------------------
+
+vim.api.nvim_set_keymap('n', '<F23>', ':set hlsearch!<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-B>', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
 
 ---------------------------- Color Scheme: Gruvbox ----------------------------
 
@@ -43,7 +70,7 @@ vim.cmd('colorscheme gruvbox-material')
 require('nvim-treesitter.configs').setup {
   ensure_installed  = { 'python', 'dockerfile', 'make', 'hcl', 'bash', 'go' },
   sync_install      = false,
-  auto_install      = true,
+  auto_install      = false,
 
   highlight = { enable = true, additional_vim_regex_highlighting = true },
 }
@@ -86,9 +113,29 @@ require('todo-comments').setup {
 
 require('lualine').setup {
   options = {
-    theme = 'gruvbox-material',
+    theme = 'gruvbox_dark',
+    component_separators = '|',
+    section_separators = { left = '', right = '' },
     disabled_filetypes = { 'packer', 'NvimTree' } -- hide status bar when using Nvim-Tree
-  }
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'filename', 'branch'},
+    lualine_c = {'fileformat'},
+    lualine_x = {'diff', 'diagnostics'},
+    lualine_y = {'filetype', 'progress'},
+    lualine_z = {'location'},
+  },
+  inactive_sections = {
+    lualine_a = { 'filename' },
+    lualine_b = {},
+    lualine_c = {},
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = { 'location' },
+  },
+  tabline = {},
+  extensions = {},
 }
 
 ------------------------- File explorer: nvim-tree.lua -------------------------
@@ -125,8 +172,12 @@ require('nvim-tree').setup({
       },
     },
   },
+  git = {
+    enable = true,
+  },
   renderer = {
     group_empty = false,
+    highlight_git = true,
   },
   filters = {
     dotfiles = true,
@@ -141,6 +192,33 @@ vim.g.python3_host_prog               = '~/.pyenv/shims/python'
 vim.g.virtualenv_directory            = 'venv'
 vim.g.python_pep8_indent_hang_closing = 0
 
+------------------------------ Highligth: Colors ------------------------------
+
+require('nvim-highlight-colors').setup {}
+
+--------------------------- Highligth: Indent lines ---------------------------
+
+vim.cmd [[highlight IndentBlanklineIndent1 guifg=#fb4934 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent2 guifg=#b8bb26 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent3 guifg=#fabd2f gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent4 guifg=#458588 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent5 guifg=#b16286 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent6 guifg=#689d6a gui=nocombine]]
+
+vim.opt.list = true
+vim.opt.listchars:append "space:⋅"
+
+require("indent_blankline").setup {
+  char_highlight_list = {
+    "IndentBlanklineIndent1",
+    "IndentBlanklineIndent2",
+    "IndentBlanklineIndent3",
+    "IndentBlanklineIndent4",
+    "IndentBlanklineIndent5",
+    "IndentBlanklineIndent6",
+  },
+}
+
 --------------------------- Package Manager: Packer ---------------------------
 
 return require('packer').startup(function(use)
@@ -148,11 +226,23 @@ return require('packer').startup(function(use)
 	use 'sainnhe/gruvbox-material'
   use 'folke/todo-comments.nvim'
   use 'nvim-lualine/lualine.nvim'
-  use {'neoclide/coc.nvim', branch = 'release'}
-	use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+  use 'lukas-reineke/indent-blankline.nvim'
+  use 'brenoprata10/nvim-highlight-colors'
+  use {
+    'neoclide/coc.nvim',
+    branch = 'release'
+  }
+	use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate'
+  }
   use {
     'nvim-tree/nvim-tree.lua',
     requires = { 'nvim-tree/nvim-web-devicons' },
     tag = 'nightly'
+  }
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+    requires = {'nvim-lua/plenary.nvim'}
   }
 end)
